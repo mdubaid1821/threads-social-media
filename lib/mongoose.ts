e51d9b1134f7d635de4a8add919559a16dca32b5
DarkStar <mdubaid1821@gmail.com>
2023-09-14 14:22:26 +0530

    modified:   lib/mongoose.ts

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,6 +1,19 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 
-export const ConnectDatabase = async()=> {
-    await mongoose.connect(process.env.DB_URI, {dbName:"threads"}).then(()=>{console.log(`Database is connected`)}).catch((error)=>{console.log(`${error}`)})
-}
+export const ConnectDatabase = async () => {
+    const dbUri = process.env.DB_URI;
+  
+    if (!dbUri) {
+      console.error("DB_URI environment variable is not defined.");
+      return;
+    }
+  
+    try {
+      await mongoose.connect(dbUri, { dbName: "threads" });
+      console.log("Database is connected");
+    } catch (error) {
+      console.error(`${error}`);
+    }
+  };
+  
